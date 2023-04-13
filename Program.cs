89bde6e9125ff4dde67df4cd3bf9b19d5686b351
Aakankshap16 <aakankshap971@gmail.com>
2023-04-14 02:22:59 +0530

    Greeting msg from UC10-13

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistrationlambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration it contain UC10 - UC13");
+        }
+    }
+}
